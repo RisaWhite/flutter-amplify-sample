cc845f7b0cc6b5a0659291141a9ca834add2d730
nico <y.elucidator@gmail.com>
2022-08-03 19:20:16 +0900

    モジュール３変更適用

--- lib/auth_service.dart
+++ lib/auth_service.dart
@@ -1,6 +1,9 @@
 import 'dart:async';
 
-import 'package:app/auth_credentials.dart';
+import 'auth_credentials.dart';
+import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
+import 'package:amplify_flutter/amplify_flutter.dart';
+import 'package:logger/logger.dart';
 
 // 1
 enum AuthFlowStatus { login, signUp, verification, session }
@@ -16,6 +19,8 @@ class AuthState {
 class AuthService {
   // 4
   final authStateController = StreamController<AuthState>();
+
+  late AuthCredentials _credentials;
 
   // 5
   void showSignUp() {
@@ -36,18 +41,77 @@ class AuthService {
   }
 
 // 2
-  void signUpWithCredentials(SignUpCredentials credentials) {
-    final state = AuthState(authFlowStatus: AuthFlowStatus.verification);
-    authStateController.add(state);
+  void signUpWithCredentials(SignUpCredentials credentials) async {
+    try {
+      // 2
+      final userAttributes = <CognitoUserAttributeKey, String>{
+        CognitoUserAttributeKey.email: credentials.email
+      };
+
+      // 3
+      final result = await Amplify.Auth.signUp(
+          username: credentials.username,
+          password: credentials.password,
+          options: CognitoSignUpOptions(userAttributes: userAttributes));
+
+      // 4
+      if (result.isSignUpComplete) {
+        loginWithCredentials(credentials);
+      } else {
+        // 5
+        _credentials = credentials;
+
+        // 6
+        final state = AuthState(authFlowStatus: AuthFlowStatus.verification);
+        authStateController.add(state);
+      }
+
+      // 7
+    } on AuthException catch (authError) {
+      Logger().e('Failed to sign up - ${authError.message}');
+    }
   }
 
-  void verifyCode(String verificationCode) {
-    final state = AuthState(authFlowStatus: AuthFlowStatus.session);
-    authStateController.add(state);
+// 1
+  void verifyCode(String verificationCode) async {
+    try {
+      // 2
+      final result = await Amplify.Auth.confirmSignUp(
+          username: _credentials.username, confirmationCode: verificationCode);
+
+      // 3
+      if (result.isSignUpComplete) {
+        loginWithCredentials(_credentials);
+      } else {
+        // 4
+        // Follow more steps
+      }
+    } on AuthException catch (authError) {
+      Logger().e('Could not verify code - ${authError.message}');
+    }
   }
 
-  void logOut() {
-    final state = AuthState(authFlowStatus: AuthFlowStatus.login);
-    authStateController.add(state);
+  void logOut() async {
+    try {
+      // 1
+      await Amplify.Auth.signOut();
+
+      // 2
+      showLogin();
+    } on AuthException catch (authError) {
+      Logger().e('Could not log out - ${authError.message}');
+    }
+  }
+
+  void checkAuthStatus() async {
+    try {
+      await Amplify.Auth.fetchAuthSession();
+
+      final state = AuthState(authFlowStatus: AuthFlowStatus.session);
+      authStateController.add(state);
+    } catch (_) {
+      final state = AuthState(authFlowStatus: AuthFlowStatus.login);
+      authStateController.add(state);
+    }
   }
 }
